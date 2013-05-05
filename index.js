6c1b4eac551312c9234cd5623c41e1441841b2e3
Romain Salles <romain.salles@outlook.com>
2013-05-06 01:20:00 +0200

    Use expressjs to manage routes

--- index.js
+++ index.js
@@ -8,45 +8,4 @@ var broadcastServer = new BroadcastServer(8128);
 // Web server
 // =============================================================================
 
-var http = require('http'),
-    url  = require('url');
-
-// page rendering
-// -----------------------------------------------------------------------------
-
-function executeSpecs(request, response) {
-  var onNewSpec = function(description, totalCount, passedCount, failedCount, passed) {
-    response.write('SPEC : ' + JSON.stringify([description, totalCount, passedCount, failedCount, passed]) + '<br/>');
-  };
-  var onNewSuite = function(description, totalCount, passedCount, failedCount) {
-    response.write('SUITE : ' + JSON.stringify([description, totalCount, passedCount, failedCount]) + '<br/>');
-  };
-  var onEndSpecs = function(description, totalCount, passedCount, failedCount) {
-    response.end();
-  };
-  // execute specs
-  broadcastServer.runSpecs(['specs/example_specs.js'], onNewSpec, onNewSuite, onEndSpecs);
-
-  // write response
-  response.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-}
-
-function render404(request, response) {
-  response.writeHead(404);
-  response.end('Error 404');
-}
-
-// server
-// -----------------------------------------------------------------------------
-
-var runSpecsRegex = new RegExp('^/specs/execute/?$');
-
-var webServer = http.createServer(function(request, response) {
-  var pathname = url.parse(request.url).pathname;
-
-  if (runSpecsRegex.test(pathname)) { executeSpecs(request, response); }
-  else                              { render404(request, response); }
-});
-
-webServer.listen(8666);
-console.log('Listening on http://localhost:8666');
+var webServer = new require('./server/TispecServer').TispecServer();
---
+++ server/TispecServer.js
@@ -0,0 +1,34 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express'),
+    http    = require('http'),
+    path    = require('path');
+
+
+function TispecServer() {
+  var app = express();
+
+  // all environments
+  app.set('port', process.env.PORT || 8666);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, '/public')));
+
+  // development only
+  if ('development' == app.get('env')) {
+    app.use(express.errorHandler());
+  }
+
+  http.createServer(app).listen(app.get('port'), function(){
+    console.log('Server listening on port ' + app.get('port'));
+  });
+}
+exports.TispecServer = TispecServer;
