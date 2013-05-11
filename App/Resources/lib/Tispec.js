71ed8529ca8f8af38bd756a56b4dbd459e0c6a30
Romain Salles <romain.salles@outlook.com>
2013-05-11 20:40:44 +0200

    Improve scope management

--- App/Resources/lib/Tispec.js
+++ App/Resources/lib/Tispec.js
@@ -5,7 +5,12 @@ var initialized = false;
 Ti.include('lib/jasmine.js');
 
 function initializeJasmine(reporter) {
-  var env = jasmine.getEnv();
+  // [Hack] force a new env.
+  // Avoid multiple execution of the same spec if the specs are run
+  // multiple times (and thus, included more than once)
+  // @see https://github.com/pivotal/jasmine/pull/357
+  // @see https://github.com/pivotal/jasmine/blob/master/src/core/base.js
+  var env = jasmine.currentEnv_ = new jasmine.Env();
 
   env.addReporter(reporter);
   env.specFilter = function(spec) {
@@ -22,19 +27,22 @@ exports.initialize = function(host, port) {
   now = nowjs.nowInitialize('//' + host + ':' + port, {});
 
   now.ready(function () {
-    var Reporter   = require('lib/TispecReporter').TispecReporter,
-        reporter   = new Reporter(now),
-        jasmineEnv = initializeJasmine(reporter);
+    var Reporter   = require('lib/TispecReporter').TispecReporter;
 
     now.execute = function (specs, conf) {
-      Ti.include('/lib/TispecHelper.js');
+      (function() {
+        var reporter   = new Reporter(now),
+            jasmineEnv = initializeJasmine(reporter);
 
-      _.each(specs, function(spec) { Ti.include(spec); });
+        Ti.include('/lib/TispecHelper.js');
 
-      if (conf.filter) { reporter.setSpecFilter(conf.filter); }
-      else        { reporter.removeSpecFilter();    }
+        _.each(specs, function(spec) { Ti.include(spec); });
 
-      jasmineEnv.execute();
+        if (conf.filter) { reporter.setSpecFilter(conf.filter); }
+        else        { reporter.removeSpecFilter();    }
+
+        jasmineEnv.execute();
+      })();
     };
   });
 
