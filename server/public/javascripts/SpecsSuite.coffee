2fd5e600c8f4a6b0a217f635bab435862b33505a
Romain Salles <romain.salles@outlook.com>
2013-05-15 00:40:45 +0200

    Refactor dashboard : move spec data in a Spec class

--- server/public/javascripts/SpecsSuite.coffee
+++ server/public/javascripts/SpecsSuite.coffee
@@ -1,19 +1,56 @@
+# SpecsSuite
+# ==============================================================================
+
 class SpecsSuite
   specs       = []
   suites      = []
   passedCount: 0
-  errorCount: 0
+  errorCount:  0
 
   constructor: (@id, @appName, @deviceName, @totalCount) ->
 
   addSpec: (spec) ->
     specs.push(spec)
+    spec.setSpecsSuite(this)
+
     if spec.passed
       @passedCount += 1
     else
-      @errorCount += 1
-    return this
+      @errorCount  += 1
+
+    updateAvancement(this)
+    return spec
 
   addSuite: (suite) ->
     suites.push(suite)
+    return this
+
+  getSpec: (id) ->
+    _.find specs, (spec) -> return spec.id is id
+
+
+# Specs
+# ==============================================================================
+class Spec
+  constructor:   (@id, @suiteName, @description) ->
+
+  setSpecsSuite: (@specsSuite) ->
+
+  setResult:     (@totalCount, @passedCount, @failedCount, @passed, @subSpecs) ->
+
+  showResults: ->
+    errorMessages = []
+    for subSpec, i in @subSpecs
+      unless subSpec.passed_
+        errorMessages.push '(' + (i+1) + ') expected <b>' + JSON.stringify(subSpec.actual) + '</b> to be <b>' + JSON.stringify(subSpec.expected) + '</b>'
+
+    className = if @passed then 'success' else 'error'
+    row = "<tr class=\"spec_row #{className}\""
+    if errorMessages.length > 0
+      subSpecsErrors = "<ul><li>#{errorMessages.join('</li><li>')}</li></ul>"
+      row += " data-title=\"Errors\" data-content=\"#{subSpecsErrors}\" data-placement=\"top\" data-html=\"true\""
+
+    row += "><td>#{@suiteName} #{@description}</td><td>#{@passedCount}/#{@totalCount}</td></tr>"
+    $(row).prependTo "#specs_results_#{@specsSuite.id}"
+
     return this
--- server/public/javascripts/SpecsSuite.js
+++ server/public/javascripts/SpecsSuite.js
@@ -1,4 +1,6 @@
-var SpecsSuite = (function() {
+var Spec, SpecsSuite;
+
+SpecsSuite = (function() {
   var specs, suites;
 
   specs = [];
@@ -18,12 +20,14 @@ var SpecsSuite = (function() {
 
   SpecsSuite.prototype.addSpec = function(spec) {
     specs.push(spec);
+    spec.setSpecsSuite(this);
     if (spec.passed) {
       this.passedCount += 1;
     } else {
       this.errorCount += 1;
     }
-    return this;
+    updateAvancement(this);
+    return spec;
   };
 
   SpecsSuite.prototype.addSuite = function(suite) {
@@ -31,7 +35,57 @@ var SpecsSuite = (function() {
     return this;
   };
 
+  SpecsSuite.prototype.getSpec = function(id) {
+    return _.find(specs, function(spec) {
+      return spec.id === id;
+    });
+  };
+
   return SpecsSuite;
 
 })();
 
+Spec = (function() {
+  function Spec(id, suiteName, description) {
+    this.id = id;
+    this.suiteName = suiteName;
+    this.description = description;
+  }
+
+  Spec.prototype.setSpecsSuite = function(specsSuite) {
+    this.specsSuite = specsSuite;
+  };
+
+  Spec.prototype.setResult = function(totalCount, passedCount, failedCount, passed, subSpecs) {
+    this.totalCount = totalCount;
+    this.passedCount = passedCount;
+    this.failedCount = failedCount;
+    this.passed = passed;
+    this.subSpecs = subSpecs;
+  };
+
+  Spec.prototype.showResults = function() {
+    var className, errorMessages, i, row, subSpec, subSpecsErrors, _i, _len, _ref;
+
+    errorMessages = [];
+    _ref = this.subSpecs;
+    for (i = _i = 0, _len = _ref.length; _i < _len; i = ++_i) {
+      subSpec = _ref[i];
+      if (!subSpec.passed_) {
+        errorMessages.push('(' + (i + 1) + ') expected <b>' + JSON.stringify(subSpec.actual) + '</b> to be <b>' + JSON.stringify(subSpec.expected) + '</b>');
+      }
+    }
+    className = this.passed ? 'success' : 'error';
+    row = "<tr class=\"spec_row " + className + "\"";
+    if (errorMessages.length > 0) {
+      subSpecsErrors = "<ul><li>" + (errorMessages.join('</li><li>')) + "</li></ul>";
+      row += " data-title=\"Errors\" data-content=\"" + subSpecsErrors + "\" data-placement=\"top\" data-html=\"true\"";
+    }
+    row += "><td>" + this.suiteName + " " + this.description + "</td><td>" + this.passedCount + "/" + this.totalCount + "</td></tr>";
+    $(row).prependTo("#specs_results_" + this.specsSuite.id);
+    return this;
+  };
+
+  return Spec;
+
+})();
